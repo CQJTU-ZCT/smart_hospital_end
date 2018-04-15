53c12bb313c8ab1975f0f667b01618823f9c39fa
mevur <991146900@qq.com>
2018-04-15 22:41:37 +0800

    emoji service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ChatDoctorComponent } from './chat-doctor/chat-doctor.component';
-import { InTimeChartComponent } from './in-time-chart/in-time-chart.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { FindPasswordPageComponent } from './find-password-page/find-password-page.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { EmailTipComponent } from './email-tip/email-tip.component';
-import { ResetPawdComponent } from './reset-pawd/reset-pawd.component';
-import { FooterLightComponent } from './footer-light/footer-light.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
-import { DoctorManageComponent } from './doctor-manage/doctor-manage.component';
-import { CarManageComponent } from './car-manage/car-manage.component';
-import { PaySysComponent } from './pay-sys/pay-sys.component';
-import { UserCenterComponent } from './user-center/user-center.component';
-import { NavVerticalComponent } from './nav-vertical/nav-vertical.component';
-import { ListViwComponent } from './list-viw/list-viw.component';
-import { DoctorService } from './services/doctor.service';
-import { DoctorInfoComponent } from './doctor-info/doctor-info.component';
-import { AddDoctorComponent } from './add-doctor/add-doctor.component';
-import { EventService } from './services/event.service';
-import { GridviewComponent } from './gridview/gridview.component';
-import { GridviewService } from './services/gridview.service';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { HelpComponent } from './help/help.component';
-import { DoctorWorkspaceComponent } from './doctor-workspace/doctor-workspace.component';
-import { ConfigService } from './services/config.service';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {ChatDoctorComponent} from './chat-doctor/chat-doctor.component';
+import {InTimeChartComponent} from './in-time-chart/in-time-chart.component';
+import {FooterComponent} from './footer/footer.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {FindPasswordPageComponent} from './find-password-page/find-password-page.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {EmailTipComponent} from './email-tip/email-tip.component';
+import {ResetPawdComponent} from './reset-pawd/reset-pawd.component';
+import {FooterLightComponent} from './footer-light/footer-light.component';
+import {ContactPageComponent} from './contact-page/contact-page.component';
+import {DoctorManageComponent} from './doctor-manage/doctor-manage.component';
+import {CarManageComponent} from './car-manage/car-manage.component';
+import {PaySysComponent} from './pay-sys/pay-sys.component';
+import {UserCenterComponent} from './user-center/user-center.component';
+import {NavVerticalComponent} from './nav-vertical/nav-vertical.component';
+import {ListViwComponent} from './list-viw/list-viw.component';
+import {DoctorService} from './services/doctor.service';
+import {DoctorInfoComponent} from './doctor-info/doctor-info.component';
+import {AddDoctorComponent} from './add-doctor/add-doctor.component';
+import {EventService} from './services/event.service';
+import {GridviewComponent} from './gridview/gridview.component';
+import {GridviewService} from './services/gridview.service';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {HelpComponent} from './help/help.component';
+import {DoctorWorkspaceComponent} from './doctor-workspace/doctor-workspace.component';
+import {ConfigService} from './services/config.service';
+import {EmojiService} from "./emoji.service";
+import {EmojiComponent} from './emoji/emoji.component';
 
 declare var require: any;
 
@@ -39,24 +41,24 @@ require('../../node_modules/jquery/dist/jquery.js');
 require('../../node_modules/popper.js/dist/popper.js');
 require('../../node_modules/bootstrap/dist/js/bootstrap.js');
 require('../../node_modules/select2/dist/js/select2.js');
-require('../../node_modules/handsontable/dist/handsontable.full.js');
 require('../../node_modules/markdown/lib/markdown.js');
 
 const appRoutes: Routes = [
-  { path: 'find', component: FindPasswordPageComponent },
-  { path: 'tip', component: EmailTipComponent },
-  { path: 'reset', component: ResetPawdComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'home', component: HomePageComponent },
-  { path: 'contact', component: ContactPageComponent },
-  { path: 'doctor', component: DoctorManageComponent },
-  { path: 'add-doctor', component: AddDoctorComponent, outlet: 'aux' },
-  { path: 'car', component: CarManageComponent },
-  { path: 'pay', component: PaySysComponent },
-  { path: 'about', component: AboutUsComponent },
-  { path: 'help', component: HelpComponent },
-  { path: 'main', component: DoctorWorkspaceComponent },
-  { path: '**', component: ErrorPageComponent }
+  {path: 'find', component: FindPasswordPageComponent},
+  {path: 'tip', component: EmailTipComponent},
+  {path: 'reset', component: ResetPawdComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'doctor', component: DoctorManageComponent},
+  {path: 'add-doctor', component: AddDoctorComponent, outlet: 'aux'},
+  {path: 'car', component: CarManageComponent},
+  {path: 'pay', component: PaySysComponent},
+  {path: 'about', component: AboutUsComponent},
+  {path: 'help', component: HelpComponent},
+  {path: 'main', component: DoctorWorkspaceComponent},
+  {path: 'emoji', component: EmojiComponent},
+  {path: '**', component: ErrorPageComponent}
 ];
 
 const interact = require('interactjs');
@@ -87,16 +89,17 @@ const interact = require('interactjs');
     GridviewComponent,
     AboutUsComponent,
     HelpComponent,
-    DoctorWorkspaceComponent
+    DoctorWorkspaceComponent,
+    EmojiComponent
   ],
   imports: [
     RouterModule.forRoot(
-    appRoutes
+      appRoutes
     ),
     BrowserModule,
     FormsModule,
   ],
-  providers: [DoctorService, EventService, GridviewService, ConfigService],
+  providers: [DoctorService, EventService, GridviewService, ConfigService, EmojiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
