18dd21afa160aa698b8981ff127cf6b330165992
mevur <cplzwr@live.cn>
2018-04-23 13:04:19 +0800

    config page

--- src/app/config-page/config-page.component.ts
+++ src/app/config-page/config-page.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ConfigService} from '../services/config.service';
 import * as $ from 'jquery';
+import * as Stackedit from 'stackedit-js/docs/lib/stackedit.js';
+import * as screenfull from 'screenfull/dist/screenfull.js';
 declare var require: any;
 require('slick-carousel');
 
@@ -13,12 +15,20 @@ export class ConfigPageComponent implements OnInit, AfterViewInit {
 
   nav: any;
   listItem: any;
+  stackedit: any;
+  textarea: any;
 
   constructor(
     private config: ConfigService
   ) {
     this.nav = this.config.getDefaultNavPills();
     this.listItem = ['宣传资料配置', '基础信息配置'];
+    this.stackedit = new Stackedit();
+    // Listen to StackEdit events and apply the changes to the textarea.
+    this.stackedit.on('fileChange', (file) => {
+      this.textarea.value = file.content.text;
+      $('.markdown-content').html(file.content.html);
+    });
   }
 
   ngOnInit() {
@@ -30,10 +40,35 @@ export class ConfigPageComponent implements OnInit, AfterViewInit {
       slidesToShow: 4,
       slidesToScroll: 4,
       arrows: true,
-      dots: true,
-      prevArrow: '<button type="button" class="slick-prev">Previous</button>'
+      dots: true
+    });
+    this.textarea = document.querySelector('textarea');
+  }
+
+  openFilePicker() {
+    $('#filePicker').click();
+  }
+
+  mdChange() {
+    const markdown = require('markdown').markdown;
+    $('.markdown-content').html(markdown.toHTML($('#profileInput').val()));
+  }
+
+  markdown() {
+    // Open the iframe
+    this.stackedit.openFile({
+      name: 'Filename', // with an optional filename
+      content: {
+        text: this.textarea.value // and the Markdown content.
+      }
     });
   }
 
+  fullScreenPreview() {
+    const target = $('.markdown-preview')[0];
+    if (screenfull.enabled) {
+      screenfull.toggle(target);
+    }
+  }
 
 }
